944aa3aca498b9995e96b2aeca371ad92d73883d
lucastteles <lucasteles_ccb@yahoo.com.br>
2022-06-24 16:41:59 -0300

    Adicionando Repositório Produto

---
+++ HortifrutiSF/HortifrutiSF.Domain/Repositories/IProdutoRepository.cs
@@ -0,0 +1,16 @@
+﻿using HortifrutiSF.Domain.Entidade;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HortifrutiSF.Domain.Repositories
+{
+    public interface IProdutoRepository
+    {
+        Task AdicionarProduto(Produto produto);
+        Task AtualizarProduto(Produto produto);
+        Task<Produto> ObterProdutoPorId(Guid idProduto);
+    }
+}
---
+++ HortifrutiSF/HortifrutiSF.Repo/Repositories/ProdutoRepository.cs
@@ -0,0 +1,38 @@
+﻿
+
+using HortifrutiSF.Domain.Entidade;
+using HortifrutiSF.Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HortifrutiSF.Repo.Repositories
+{
+    public class ProdutoRepository : IProdutoRepository
+    {
+        private readonly ProdutoContext _produtoContext;
+
+        public ProdutoRepository(ProdutoContext produtoContext)
+        {
+            _produtoContext = produtoContext;
+        }
+
+        public async Task AdicionarProduto(Produto produto)
+        {
+            await _produtoContext.Produto.AddAsync(produto);
+            _produtoContext.SaveChanges();
+        }
+
+        public async Task AtualizarProduto(Produto produto)
+        {
+            _produtoContext.Produto.Update(produto);
+            _produtoContext.SaveChanges();
+        }
+
+        public async Task<Produto> ObterProdutoPorId(Guid idProduto)
+        {
+            return await _produtoContext.Produto.FirstOrDefaultAsync(x => x.Id == idProduto);
+        }
+    }
+}
